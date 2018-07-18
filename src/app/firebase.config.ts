b60db86c72b65a73b7762466af143d7a7c0788db
Priyank Singh <priyank.singh@just-eat.com>
2018-07-18 15:04:29 +1000

    Initial code for adding camera functionality

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCSR3X7T0upUd2UX9sxA0uR16z-c50KMjc",
+    authDomain: "foodbook-e57ee.firebaseapp.com",
+    databaseURL: "https://foodbook-e57ee.firebaseio.com",
+    projectId: "foodbook-e57ee",
+    storageBucket: "foodbook-e57ee.appspot.com",
+    messagingSenderId: "477000375819"
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {storage, initializeApp} from 'firebase';
+import { FIREBASE_CONFIG } from '../../app/firebase.config'
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private camera:Camera, public navCtrl: NavController) {
+
+    initializeApp(FIREBASE_CONFIG);
+  }
+   async takePhoto(){
+    try {
+      const pictures = storage().ref('pictures/foodbook');
+     
+    const options: CameraOptions = {
+      quality: 50,
+      targetHeight: 600,
+      targetWidth: 600,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    const result = await this.camera.getPicture(options);
+    const image = `data:image/jpeg;base64,${result}`;
+ 
+
+    pictures.putString(image, 'data_url');
+   
+  }
+  catch(e){
 
   }
-
+  }
 }
